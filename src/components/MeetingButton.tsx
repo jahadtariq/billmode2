3fa0998011b12c66c2efebb929fc4cbb1bfacc68
jahadtariq <muhammadjahadt@gmail.com>
2024-06-25 15:54:25 +0500

    changes

--- src/components/MeetingButton.tsx
+++ src/components/MeetingButton.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const MeetingButton = () => {
   return (
-    <Link to="/" className="fixed z-50 bottom-3 right-8 lg:right-10 w-[100px] md:w-[150px] md:h-[170px] cursor-pointer bg-transparent hover:bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-lightGreen from-0% to-transparent to-50% transition-color duration-1000 ease-in-out">
+    <Link to="/" className="fixed z-50 bottom-3 right-6 lg:right-10 w-[100px] md:w-[150px] md:h-[170px] cursor-pointer bg-transparent hover:bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-lightGreen from-0% to-transparent to-50% transition-color duration-1000 ease-in-out">
         <img src="meetingbutton.svg"/>
     </Link>
   )
